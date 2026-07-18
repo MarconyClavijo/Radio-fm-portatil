35699f8a98ea4ed1756f19b947ec1f7b89a0efdc
MarconyClavijo <marconyclavijo@hotmail.com>
2026-07-18 04:51:45 -0500

    Update URL for Radio La Fuerte

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const stations = [
   {
     name: "Radio La Fuerte",
     frequency: "104.7",
-    url: "https://innovatestream.pe/7322",
+    url: "https://innovatestream.pe",
     logo: "img/lafuerte.png",
     facebook: "https://facebook.com",
     whatsapp: "https://wa.me"
